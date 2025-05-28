e3aa600787b1d90a9f5dfad6c3b0bd09a2a49c8d
KD3_89298_Sanket <sanketkhokadesk@gmail.com>
2025-05-28 18:12:04 +0530

    Adding MERN Code

---
+++ JavaScript_code/Section14_JavaScript_Basics/Updating_variable.js
@@ -0,0 +1,13 @@
+let score = 5;
+console.log(score);
+
+// Upadting the code
+score = score + 5;
+console.log(score) //10
+
+score += 5;
+console.log(score) //15
+
+score++ //16
+console.log(score)
+
---
+++ JavaScript_code/Section14_JavaScript_Basics/increment_decrement.js
@@ -0,0 +1,17 @@
+// post-increment operator
+let i= 5;
+console.log(i++); //5 
+console.log(i); //6
+
+let result = i++;
+console.log(result) //6
+console.log(i) //7
+
+// pre-increment operator
+let j = 10;
+console.log(++j); //11
+console.log(j)//11
+
+let  result2 = ++j; 
+console.log(result2); //12
+console.log(j) //12
